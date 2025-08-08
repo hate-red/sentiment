6acc001e3ac9556a00679e75bba919ac7f4f5190
hate-red <fpantshava@yandex.ru>
2025-08-08 19:07:40 +0200

    made first migration

---
+++ app/models/models.py
@@ -0,0 +1,35 @@
+from sqlalchemy import ForeignKey, Text, text, Numeric
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+
+from app.database import Base, str_uniq, int_pk, str_null_true
+
+
+class History(Base):
+    id: Mapped[int_pk]
+    content: Mapped[str] = mapped_column(Text, nullable=False)
+    avg_sentiment: Mapped[float] = mapped_column(Numeric, nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
+
+    user: Mapped['User'] =  relationship('User', back_populates='history')
+
+    def __str__(self):
+        return f'{self.content :20}, {self.avg_sentiment}'
+
+
+    def __repr__(self):
+        return str(self)
+
+
+class User(Base):
+    id: Mapped[int_pk]
+    username: Mapped[str_uniq]
+    email: Mapped[str_uniq]
+    role: Mapped[str] = mapped_column(server_default='default')
+
+
+    def __str__(self):
+        return f'{self.username}, {self.role}'
+
+
+    def __repr__(self):
+        return str(self)
